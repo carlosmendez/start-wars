18e0567115d56c1961a99f046f84d52ae6ae6fec
Carlos Mendez <carlosmendezmartin@gmail.com>
2022-10-28 11:21:05 +0200

    Add emotion and reset CSS

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { createClient } from "urql";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PersonPage from "./pages/PersonPage";
 import HomePage from "./pages/HomePage";
+import emotionReset from  'emotion-reset';
+import { Global, css } from '@emotion/react';
 
 const client = createClient({
   url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
@@ -24,6 +26,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider value={client}>
+      <Global styles={css`${emotionReset}`} />
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
